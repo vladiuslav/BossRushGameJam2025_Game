f96bc35f288c3e6a2c15a7f3c68bb306c2f78ad3
vladiuslav <vladiuslav.vladiuslav1@gmail.com>
2025-02-01 22:03:02 +0100

    Musick now works

--- Scenes/Maps/Main_Menu/main_menu.gd
+++ Scenes/Maps/Main_Menu/main_menu.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var levels_container: MarginContainer = $LevelsContainer
 @onready var settings: TextureRect = $Settings
 
+var master_bus = AudioServer.get_bus_index("Master")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -34,3 +36,11 @@ func _on_level_2_button_pressed() -> void:
 
 func _on_level_3_button_pressed() -> void:
 	Global.change_current_scene(Global.SCENES.arena_3)
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus,value)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus,true)
+	else:
+		AudioServer.set_bus_mute(master_bus,false)
