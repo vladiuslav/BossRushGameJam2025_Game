b6708ef9ce4638dcd19030ab3b2f32f99ee9928f
vladiuslav <vladiuslav.vladiuslav1@gmail.com>
2025-02-01 21:50:58 +0100

    lol works may be not

--- Scenes/Maps/Main_Menu/main_menu.gd
+++ Scenes/Maps/Main_Menu/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var levels_container: MarginContainer = $LevelsContainer
+@onready var settings: TextureRect = $Settings
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,12 +16,21 @@ func _process(delta: float) -> void:
 func _on_play_button_pressed() -> void:
 	Global.change_current_scene(Global.SCENES.dojo)
 
+func _on_select_level_button_pressed() -> void:
+	levels_container.visible = !levels_container.visible
 
 func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
-	# Add there global sound changing logic.
+	settings.visible = !settings.visible
 
-
-
-func _on_exit_button_pressed() -> void:
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_level_1_button_pressed() -> void:
+	Global.change_current_scene(Global.SCENES.arena_1)
+
+func _on_level_2_button_pressed() -> void:
+	Global.change_current_scene(Global.SCENES.arena_2)
+
+func _on_level_3_button_pressed() -> void:
+	Global.change_current_scene(Global.SCENES.arena_3)
